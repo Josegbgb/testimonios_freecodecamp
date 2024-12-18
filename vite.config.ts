74c652a3084f744894be3289166de6aa2d8d6fdf
Jose Gonzalez <josemiguelgb27@gmail.com>
2024-12-18 14:07:52 -0400

    correcciones

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Testimonios/",
+  base: process.env.VITE_BASE_PATH || "/", // Lee la variable de entorno, usa "/" por defecto
 });
